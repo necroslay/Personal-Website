286e8b2eff87b2d43b960a5742d47a30b392ad3f
necroslay <124463658+necroslay@users.noreply.github.com>
2024-01-18 18:45:48 -0500

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,41 @@
+export default {
+	"pages": {
+		"root": {
+			"name": "root",
+			"children": [
+				"65a9b7f6926d910020eb81f1",
+				"65a9b7f6926d910020eb81f5"
+			],
+			"id": "root",
+			"pageUrl": "root"
+		},
+		"65a9b7f6926d910020eb81f1": {
+			"id": "65a9b7f6926d910020eb81f1",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"65a9b7f6926d910020eb81f5": {
+			"id": "65a9b7f6926d910020eb81f5",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {
+			"background": "--color-dark"
+		},
+		"seo": {
+			"background_win10": "#232323",
+			"title": "Grand Canyon Rafting Experience",
+			"description": "Each day, you'll wake up to the smell of freshly brewed coffee, and the sounds of rushing water mingled with the murmur of sleepy voices echoing through the canyon walls.",
+			"og:title": "Grand Canyon Rafting Experience",
+			"og:description": "Each day, you'll wake up to the smell of freshly brewed coffee, and the sounds of rushing water mingled with the murmur of sleepy voices echoing through the canyon walls.",
+			"favicon_apple_152px": "https://uploads.quarkly.io/6166a2b829a0a1001e6ca5fd/images/noto_snow-capped-mountain127.png?v=2021-10-13T09:17:28.297Z",
+			"favicon_win10_270px": "https://uploads.quarkly.io/6166a2b829a0a1001e6ca5fd/images/og%20img.png?v=2021-10-13T09:16:11.796Z",
+			"og:image": "https://uploads.quarkly.io/6166a2b829a0a1001e6ca5fd/images/og%20img.png?v=2021-10-13T09:16:11.796Z",
+			"favicon_32px": "https://uploads.quarkly.io/6166a2b829a0a1001e6ca5fd/images/noto_snow-capped-mountain.png?v=2021-10-13T09:17:21.731Z"
+		}
+	}
+}
